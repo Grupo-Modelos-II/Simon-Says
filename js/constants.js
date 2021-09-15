51754176c15b0888f88cc0a95fb9a50ad87ad14b
Sergio David Paez Suarez <spaezsuarez@gmail.com>
2021-09-15 08:33:33 -0500

    Modificando texto de un modal

--- js/constants.js
+++ js/constants.js
@@ -1,11 +1,11 @@
 const modalDetail = {
     title: 'Detalles',
-    html: 'Más información en el enlace <a href="https://github.com/Grupo-Modelos-II/Simon-Says" target="_blank">repositorio</a>',
+    html: 'Más información en el enlace al siguiente <a href="https://github.com/Grupo-Modelos-II/Simon-Says" target="_blank">repositorio</a>',
 };
 
 const modalSettings = {
     title: 'Detalles',
-    html: 'Más información en el enlace <a href="https://github.com/Grupo-Modelos-II/Simon-Says" target="_blank">repositorio</a>',
+    html: '',
 };
 
 const modalLose = {
