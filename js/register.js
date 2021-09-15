b31eeb6e21cecfa61185e54e01419b905de31d0a
Sergio David Paez Suarez <spaezsuarez@gmail.com>
2021-09-15 08:33:15 -0500

    Maquetación inicial pagina de registro

---
+++ js/register.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('form-register');
+let httpClient = new HttpClient();
+
+form.addEventListener('submit',(event) => {
+    event.preventDefault();
+
+    
+    
+});
