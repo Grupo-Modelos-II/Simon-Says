60c6236122dc3d7c5a0541525b83d6f622162382
Jesús Bermúdez <jema0622@outlook.com>
2021-09-15 22:44:59 -0500

    Register works

--- js/register.js
+++ js/register.js
@@ -3,7 +3,18 @@ let httpClient = new HttpClient();
 
 form.addEventListener('submit',(event) => {
     event.preventDefault();
-
-    
-    
+    let name = document.getElementById('name').value;
+    let pass = document.getElementById('passUno').value;
+    let reqpass = document.getElementById('passDos').value;
+    if(pass === reqpass){
+        httpClient.post('/player/create', {name, pass, max_score: 0})
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    } else {
+        openDialog('Las contraseñas no coinciden');
+    }
 });
