2bafc67148f60805cd6d26161efa741ea341b349
Sergio David Paez Suarez <spaezsuarez@gmail.com>
2021-09-16 10:17:25 -0500

    Eliminando opcion

--- js/constants.js
+++ js/constants.js
@@ -3,11 +3,6 @@ const modalDetail = {
     html: 'Más información en el enlace al siguiente <a href="https://github.com/Grupo-Modelos-II/Simon-Says" target="_blank">repositorio</a>',
 };
 
-const modalSettings = {
-    title: 'Detalles',
-    html: '',
-};
-
 const modalLose = {
     text: 'Lo siento, perdiste',
     icon: 'error',
