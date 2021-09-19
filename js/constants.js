ae914ade697875ae00f0c55b4ff27d4a1d58e21e
Sergio David Paez Suarez <spaezsuarez@gmail.com>
2021-09-19 13:25:45 -0500

    19/09/2021

--- js/constants.js
+++ js/constants.js
@@ -1,6 +1,8 @@
 const modalDetail = {
     title: 'Detalles',
-    html: 'Más información en el enlace al siguiente <a href="https://github.com/Grupo-Modelos-II/Simon-Says" target="_blank">repositorio</a>',
+    html: `
+
+   <a href="https://github.com/Grupo-Modelos-II/Simon-Says" id='ref' target="_blank">repositorio</a>`,
 };
 
 const modalSettings = {
